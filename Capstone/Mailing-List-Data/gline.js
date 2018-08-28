cf55e7581aa6cfaf153c6fb624d6dc2e11156399
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2018-08-28 21:45:10 +0530

    Few changes in the data.

---
+++ Capstone/Mailing-List-Data/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','umich.edu','mac.com','berkeley.edu','unl.edu','mtu.edu','columbia.edu','ucdavis.edu','uct.ac.za','indiana.edu','ufp.pt'],
+['2005-12',51,12,12,12,11,11,11,10,9,9]
+];
